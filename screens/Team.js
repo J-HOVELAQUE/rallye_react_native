63318eae9315fb7749a4e7e5793147dc06a88381
J-HOVELAQUE <j.hovelaque@orange.fr>
2020-12-07 15:18:42 +0100

    import the state userConnected from the reduce store on TeamScreen for testing

--- screens/Team.js
+++ screens/Team.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { connect } from 'react-redux';
 
-export default function TeamScreen() {
+
+function TeamScreen(props) {
+
+  console.log('Utilisateur connecté', props.userConnected);
+
   return (
-    <View style={{ flex: 1, backgroundColor:'#e67e22', alignItems: "center",
-    justifyContent: "center"}}>
-        <Text>TeamScreen</Text>  
+    <View style={{
+      flex: 1, backgroundColor: '#e67e22', alignItems: "center",
+      justifyContent: "center"
+    }}>
+      <Text>TeamScreen</Text>
     </View>
   );
 }
+
+function mapStateToProps(state) {
+  return { userConnected: state.userConnected }
+}
+
+export default connect(
+  mapStateToProps,
+  null
+)(TeamScreen);
