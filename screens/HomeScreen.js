38411af92a81342256b7259753544e07aad4b124
J-HOVELAQUE <j.hovelaque@orange.fr>
2020-12-08 15:37:18 +0100

    fetching on the route for getting user data in db

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,13 @@
 import React, { useEffect } from 'react';
 import { StyleSheet, Text, View, ImageBackground, Button } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
+import { connect } from 'react-redux';
 
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+
+const serverUrl = 'https://powerful-earth-91256.herokuapp.com';
+// const serverUrl = 'http://192.168.1.26:3000/user/sign-up';
 
 export default function HomeScreen({ navigation }) {
 
@@ -16,6 +20,13 @@ export default function HomeScreen({ navigation }) {
         if (value !== null) {
           // value previously stored
           console.log('TOKEN', value);
+
+          const rawAnswer = await fetch(`${serverUrl}/user/get-user?token=${value}`, {
+            method: 'GET',
+          });
+          const answer = await rawAnswer.json();
+          console.log('User trouvé en db', answer);
+
         }
       } catch (e) {
         // error reading value
@@ -23,13 +34,6 @@ export default function HomeScreen({ navigation }) {
       }
     }
     getData();
-
-    // AsyncStorage.getItem(key: "token",
-    //   function (err, data) {
-    //     const token = JSON.parse(data);
-    //     console.log('TOKEN', data);
-    //   })
-
   }, [])
 
   return (
@@ -59,3 +63,19 @@ const styles = StyleSheet.create({
 
   }
 })
+
+// function mapDispatchToProps(dispatch) {
+//   return {
+//     onRecordUserConnected: function (user) {
+//       dispatch({
+//         type: 'record',
+//         user: user
+//       })
+//     }
+//   }
+// }
+
+// export default connect(
+//   null,
+//   mapDispatchToProps
+// )(LoginScreen);
