78db0f34133e76185671c060256d8dbb7b8b3c75
Manon Le <babyvert13@hotmail.com>
2020-12-08 16:29:15 +0100

    disposition des elements frontend de la page login

--- screens/Login.js
+++ screens/Login.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, ScrollView, KeyboardAvoidingView } from 'react-native';
 import { Button, Input, Overlay } from 'react-native-elements'
 import { connect } from 'react-redux';
+import { Container, Header, Content, Footer, FooterTab, Icon } from 'native-base';
+
 
 const serverUrl = 'https://powerful-earth-91256.herokuapp.com';
 // const serverUrl = 'http://192.168.1.26:3000/user/sign-up';
@@ -90,82 +92,151 @@ function LoginScreen(props) {
   }
 
   return (
+    <Container>
+        <Header>
+              <Icon onPress={() => this.props.navigation.openDrawer()}style={{color:'white'}}name="home" />
+        </Header>
+        <Content>
 
+          
+          <View style={{ flex: 1, backgroundColor: '#e67e22', alignItems: "center", justifyContent: "center" }}>
+            <Overlay isVisible={visible} onBackdropPress={() => { toggleOverlay() }}>
+              <View>
+                {errors.map(err => { return (<Text>{err}</Text>) })}
+                <Button
+                  title="OK"
+                  buttonStyle={{ backgroundColor: "#eb4d4b" }}
+                  type="solid"
+                  onPress={() => { toggleOverlay() }}
+                />
+              </View>
+            </Overlay>
 
+            {/* <ScrollView> */}
 
+              <Text style={{paddingTop:10}}>SIGN IN</Text>
+              <View style={{
+                width: '90%',
+                height: 200,
+                backgroundColor: 'gray',
+                marginBottom: '5%',
+                borderColor: 'red',
+                borderRadius: 20,
+                flex: 1,
+                alignItems: "center",
+                justifyContent: 'center', 
+                paddingLeft: 10,
+              }}>
 
-    <View style={{ flex: 1, backgroundColor: '#e67e22', alignItems: "center", justifyContent: "center" }}>
+                {/* <KeyboardAvoidingView behavior="padding" enabled> */}
+                  <Input
+                    containerStyle={{ width: '90%', height: '25%' }}
+                    inputStyle={{ marginLeft: 10 }}
+                    inputContainerStyle={{borderTopColor:'red'}}
+                    placeholder='Email'
+                    onChangeText={(val) => setEmailSignIn(val)}
+                  />
+                  <Input
+                    containerStyle={{ width: '90%', height: '25%' }}
+                    inputStyle={{ marginLeft: 10 }}
+                    placeholder='Password'
+                    onChangeText={(val) => setPasswordSignIn(val)}
+                  />
+              
+                  <Button
+                    title="Send"
+                    type="solid"
+                    containerStyle={{margin: 10}}
+                    onPress={() => {
+                      processSignIn();
+                    }}
+                  />
+                {/* </KeyboardAvoidingView> */}
+              </View>
+            
+              <Text>SIGN UP</Text>
+              <View style={{
+                width: '90%',
+                height: 350,
+                backgroundColor:'white',
+                borderRadius: 20,
+                flex: 1,
+                alignItems: "center",
+                justifyContent: 'center', 
+                paddingLeft: 10,
+                marginBottom: 10
+              }}>
 
-      <Overlay isVisible={visible} onBackdropPress={() => { toggleOverlay() }}>
-        <View>
+                {/* <KeyboardAvoidingView behavior="padding" enabled> */}
+                  <View style={{
+                    width: '80%',
+                    height: '80%',
+                    justifyContent: 'center'
+                  }}>
 
-          {errors.map(err => { return (<Text>{err}</Text>) })}
-          <Button
-            title="OK"
-            buttonStyle={{ backgroundColor: "#eb4d4b" }}
-            type="solid"
-            onPress={() => { toggleOverlay() }}
+                    <Input
+                      containerStyle={{width: '90%', height: '20%' }}
+                      inputStyle={{ marginLeft: 10 }}
+                      placeholder='First Name'
+                      onChangeText={(val) => setFirstname(val)}
+                    />
+                    <Input
+                      containerStyle={{ width: '90%', height: '20%'  }}
+                      inputStyle={{ marginLeft: 10 }}
+                      placeholder='Name'
+                      onChangeText={(val) => setName(val)}
+                    />
+                    <Input
+                      containerStyle={{ width: '90%', height: '20%'  }}
+                      inputStyle={{ marginLeft: 10 }}
+                      placeholder='Email'
+                      onChangeText={(val) => setEmail(val)}
+                    />
+                    <Input
+                      containerStyle={{ width: '90%', height: '20%'  }}
+                      inputStyle={{ marginLeft: 10 }}
+                      placeholder='Password'
+                      onChangeText={(val) => setPassword(val)}
+                    />
+                  </View>
 
-          />
-        </View>
-      </Overlay>
+                  <Button
+                    title="Send"
+                    type="solid"
+                    onPress={() => { processSignUp() }}
+                  />
+                {/* </KeyboardAvoidingView> */}
+              </View>
 
+            {/* </ScrollView > */}
+          </View>
+        </Content>
 
-      <Text>SIGN IN</Text>
-      <Input
-        containerStyle={{ marginBottom: 25, width: '70%' }}
-        inputStyle={{ marginLeft: 10 }}
-        placeholder='Email'
-        onChangeText={(val) => setEmailSignIn(val)}
-      />
-      <Input
-        containerStyle={{ marginBottom: 25, width: '70%' }}
-        inputStyle={{ marginLeft: 10 }}
-        placeholder='Password'
-        onChangeText={(val) => setPasswordSignIn(val)}
-      />
-      <Button
-        title="Send"
-        type="solid"
-        onPress={() => {
-          processSignIn();
-
-        }}
-      />
-
-      <Text>SIGN UP</Text>
-      <Input
-        containerStyle={{ marginBottom: 25, width: '70%' }}
-        inputStyle={{ marginLeft: 10 }}
-        placeholder='First Name'
-        onChangeText={(val) => setFirstname(val)}
-      />
-      <Input
-        containerStyle={{ marginBottom: 25, width: '70%' }}
-        inputStyle={{ marginLeft: 10 }}
-        placeholder='Name'
-        onChangeText={(val) => setName(val)}
-      />
-      <Input
-        containerStyle={{ marginBottom: 25, width: '70%' }}
-        inputStyle={{ marginLeft: 10 }}
-        placeholder='Email'
-        onChangeText={(val) => setEmail(val)}
-      />
-      <Input
-        containerStyle={{ marginBottom: 25, width: '70%' }}
-        inputStyle={{ marginLeft: 10 }}
-        placeholder='Password'
-        onChangeText={(val) => setPassword(val)}
-      />
-
-      <Button
-        title="Send"
-        type="solid"
-        onPress={() => { processSignUp() }}
-      />
-    </View>
-    
+        <Footer>
+          <FooterTab style={{backgroundColor: '#313131',}}>
+            <Button onPress={() => this.props.navigation.navigate('Home')}>
+              <Icon style={{color:'white'}} name="home" />
+              <Text style={{color:'white'}}>Home</Text>
+            </Button>
+            <Button onPress={() => this.props.navigation.navigate('Team')}>
+              <Icon style={{color:'white'}} name="car" />
+              <Text style={{color:'white'}}>Team</Text>
+            </Button>
+            <Button onPress={() => this.props.navigation.navigate('Classement')}>
+              <Icon style={{color:'white'}} name="add" />
+              <Text style={{color:'white'}}>Podium</Text>
+            </Button >
+            <Button onPress={() => this.props.navigation.navigate('Map')}>
+              <Icon style={{color:'white'}}name="map" />
+              <Text style={{color:'white'}}>Map</Text>
+            </Button>
+            <Button onPress={() => this.props.navigation.openDrawer()}>
+            <Icon style={{color:'white'}}name="menu"  />
+            <Text style={{color:'white'}}>Menu</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
   );
 }
 
