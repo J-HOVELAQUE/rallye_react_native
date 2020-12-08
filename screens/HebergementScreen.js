abf3ca14fb42a346173da8302a4150acb855e3ff
J-HOVELAQUE <j.hovelaque@orange.fr>
2020-12-08 18:13:20 +0100

    socket and map ok

--- screens/HebergementScreen.js
+++ screens/HebergementScreen.js
@@ -8,51 +8,52 @@ const dataArray = [
   { title: "Navette", content: "Lorem ipsum dolor sit amet" }
 ];
 
-export default class test extends Component {
-  render() {
-    return (
-      <Container>
-        <Header>
-              <Icon onPress={() => this.props.navigation.openDrawer()}style={{color:'white'}}name="home" />
-        </Header>
-        <Content>
-          <Accordion
-            dataArray={dataArray}
-            icon="add"
-            expandedIcon="remove"
-            iconStyle={{ color: "black" }}
-            expandedIconStyle={{ color: "red" }}
-          />
+export default function HebergementScreen(props) {
 
 
-        </Content>
-        <Footer>
-          <FooterTab style={{backgroundColor: '#313131',}}>
-            <Button onPress={() => this.props.navigation.navigate('Home')}>
-              <Icon style={{color:'white'}} name="home" />
-              <Text style={{color:'white'}}>Home</Text>
-            </Button>
-            <Button onPress={() => this.props.navigation.navigate('Team')}>
-              <Icon style={{color:'white'}} name="car" />
-              <Text style={{color:'white'}}>Team</Text>
-            </Button>
-            <Button onPress={() => this.props.navigation.navigate('Classement')}>
-              <Icon style={{color:'white'}} name="add" />
-              <Text style={{color:'white'}}>Podium</Text>
-            </Button >
-            <Button onPress={() => this.props.navigation.navigate('Map')}>
-              <Icon style={{color:'white'}}name="map" />
-              <Text style={{color:'white'}}>Map</Text>
-            </Button>
-            <Button onPress={() => this.props.navigation.openDrawer()}>
-            <Icon style={{color:'white'}}name="menu"  />
-            <Text style={{color:'white'}}>Menu</Text>
-            </Button>
-          </FooterTab>
-        </Footer>
-      </Container>
-    );
-  }
+  return (
+    <Container>
+      <Header>
+        <Icon onPress={() => props.navigation.openDrawer()} style={{ color: 'white' }} name="home" />
+      </Header>
+      <Content>
+        <Accordion
+          dataArray={dataArray}
+          icon="add"
+          expandedIcon="remove"
+          iconStyle={{ color: "black" }}
+          expandedIconStyle={{ color: "red" }}
+        />
+
+
+      </Content>
+      <Footer>
+        <FooterTab style={{ backgroundColor: '#313131', }}>
+          <Button onPress={() => props.navigation.navigate('Home')}>
+            <Icon style={{ color: 'white' }} name="home" />
+            <Text style={{ color: 'white' }}>Home</Text>
+          </Button>
+          <Button onPress={() => props.navigation.navigate('Team')}>
+            <Icon style={{ color: 'white' }} name="car" />
+            <Text style={{ color: 'white' }}>Team</Text>
+          </Button>
+          <Button onPress={() => props.navigation.navigate('Classement')}>
+            <Icon style={{ color: 'white' }} name="add" />
+            <Text style={{ color: 'white' }}>Podium</Text>
+          </Button >
+          <Button onPress={() => props.navigation.navigate('Map')}>
+            <Icon style={{ color: 'white' }} name="map" />
+            <Text style={{ color: 'white' }}>Map</Text>
+          </Button>
+          <Button onPress={() => props.navigation.openDrawer()}>
+            <Icon style={{ color: 'white' }} name="menu" />
+            <Text style={{ color: 'white' }}>Menu</Text>
+          </Button>
+        </FooterTab>
+      </Footer>
+    </Container>
+  );
 }
-function MenuScreen ({navigation}){}
+
+
 
