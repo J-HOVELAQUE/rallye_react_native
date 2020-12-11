69ddb3af4b442c5e372d69dd306e4a3b32170868
J-HOVELAQUE <j.hovelaque@orange.fr>
2020-12-11 15:53:56 +0100

    filtering favorite on the map

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
+import { withNavigationFocus } from 'react-navigation';
 
-import { Container, Header, Left, Body, Right, Button, Icon, Title,Card,CardItem,Content } from 'native-base';
+
+import { Container, Header, Left, Body, Right, Button, Icon, Title, Card, CardItem, Content } from 'native-base';
 import MapView, { Marker } from 'react-native-maps';
 
 import socketIOClient from "socket.io-client";
@@ -11,56 +14,90 @@ const serverUrl = 'https://powerful-earth-91256.herokuapp.com';
 
 const socket = socketIOClient(serverUrl);
 
-export default function MapScreen(props) {
+function MapScreen(props) {
 
-    const [vehiculeToDisplay, setVehiculeToDisplay] = useState([]);
 
-    useEffect(() => {
-        socket.on('sendPositionToAll', (msg) => {
-            setVehiculeToDisplay(msg.allPosition)
+  const [vehiculeToDisplay, setVehiculeToDisplay] = useState([]);
+  const [userFavorites, setUserFavorites] = useState([]);
 
-        }), []
-    })
+  // if (props.isFocused) {
+  //   const favList = props.userFavorites.map(fav => fav.car_id);
+  //   setUserFavorites(favList);
+  // }
 
-    const markerVehicules = vehiculeToDisplay.map((car, i) => {
-        return <Marker
-            coordinate={{ latitude: car.lat, longitude: car.long }}
-            title={car.idVehicule.toString()}
-            key={i}
-        />
-    })
 
-    return (
-        <View style={{ flex: 1 }}>
+  useEffect(() => {
+    socket.on('sendPositionToAll', (msg) => {
+      setVehiculeToDisplay(msg.allPosition)
 
-        <Header style={{ backgroundColor: '#313131'}}>
-          <Left>
-            <Button transparent>
-              <Icon name='ios-people' />
-            </Button>
-          </Left>
-          <Body>
-            <Title>Map</Title>
-          </Body>
-          <Right>
-            <Button transparent onPress={() => props.navigation.openDrawer()}>
-              <Icon name='menu' />
-            </Button>
-          </Right>
-        </Header>
+    }), []
+  });
 
-            < MapView style={{ flex: 1 }
-            }
-                initialRegion={{
-                    latitude: 48.866667,
-                    longitude: 2.333333,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
-                }}
-            >
-                {markerVehicules}
-            </MapView >
+  useEffect(() => {
+    const favList = props.userFavorites.map(fav => fav.car_id);
+    setUserFavorites(favList);
+  }, [props.userFavorites]);
 
-        </View>
-    );
+
+  const displayWithFavorite = vehiculeToDisplay.filter(car => userFavorites.includes(car.idVehicule.toString()));
+
+  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>> DATA', displayWithFavorite);
+
+  const markerVehicules = displayWithFavorite.map((car, i) => {
+    return <Marker
+      coordinate={{ latitude: car.lat, longitude: car.long }}
+      title={car.idVehicule.toString()}
+      key={i}
+    />
+  })
+
+  return (
+    <View style={{ flex: 1 }}>
+
+      <Header style={{ backgroundColor: '#313131' }}>
+        <Left>
+          <Button transparent>
+            <Icon name='ios-people' />
+          </Button>
+        </Left>
+        <Body>
+          <Title>Map</Title>
+        </Body>
+        <Right>
+          <Button transparent onPress={() => props.navigation.openDrawer()}>
+            <Icon name='menu' />
+          </Button>
+        </Right>
+      </Header>
+
+      < MapView style={{ flex: 1 }
+      }
+        initialRegion={{
+          latitude: 48.866667,
+          longitude: 2.333333,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        {markerVehicules}
+      </MapView >
+
+    </View>
+  );
 }
+
+function mapStateToProps(state) {
+  return {
+    userFavorites: state.userFavorites,
+  }
+}
+
+// export default connect(
+//   mapStateToProps,
+//   null
+// )(MapScreen);
+
+export default withNavigationFocus(connect(
+  mapStateToProps,
+  null
+)(MapScreen))
