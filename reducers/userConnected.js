5485b76799a01168321345d45362beb24265eb5d
Manon Le <babyvert13@hotmail.com>
2020-12-11 16:23:59 +0100

    real data in monCompte. Next step: edit infos

--- reducers/userConnected.js
+++ reducers/userConnected.js
@@ -2,7 +2,12 @@ export default function (userConnected = {}, action) {
     if (action.type == 'record') {
         return {
             token: action.user.token,
-            status: action.user.status
+            status: action.user.status,
+            firstName: action.user.firstname,
+            lastName: action.user.name,
+            email: action.user.email,
+            avatar: action.user.avatar,
+            nationality : action.user.nationality
         };
     } else {
         return userConnected;
--- screens/monCompte.js
+++ screens/monCompte.js
@@ -1,60 +1,79 @@
 import React from 'react';
-import { Container, Header, Content, Footer, FooterTab, Button, Icon ,Left,Body,Title,Right, Image  } from 'native-base';
-import { View, Text ,ImageBackground,StyleSheet} from 'react-native';
+import { Container, Header, Content, Footer, FooterTab, Button, Icon, Left, Body, Title, Right, Image } from 'native-base';
+import { View, Text, ImageBackground, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { connect } from 'react-redux'
 
 
-export default function ProgrammeScreen(props) {
+function monCompteScreen(props) {
+
+  console.log("COMPTE ///// :", props.userConnected)
+  const user = props.userConnected
 
   return (
     <Container >
-      
-      <Header style={{ backgroundColor: '#313131'}}>
-        
-          <Left>
-            <Button transparent>
+
+      <Header style={{ backgroundColor: '#313131' }}>
+
+        <Left>
+          <Button transparent>
             <Icon name='ios-people' />
-            </Button>
-          </Left>
-          <Body>
-            <Title>Mon Compte</Title>
-          </Body>
-          <Right>
-            <Button transparent onPress={() => props.navigation.openDrawer()}>
-              <Icon name='menu' />
-            </Button>
-          </Right>
-        </Header>
-<ImageBackground source={require('../assets/fondCarbon.jpg')} style={styles.container}>
-      <Content>
-      
+          </Button>
+        </Left>
+        <Body>
+          <Title>Mon Compte</Title>
+        </Body>
+        <Right>
+          <Button transparent onPress={() => props.navigation.openDrawer()}>
+            <Icon name='menu' />
+          </Button>
+        </Right>
+      </Header>
+      <ImageBackground style={styles.container}>
+
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          
+
+          <Text>Avatar: {user.avatar !== undefined ? user.nationality : 'non renseignée' }</Text>
+          <Button><Text>Changer mon avatar</Text></Button>
+
+          <Text>Prenom: {user.firstName}</Text>
+          <Button><Text>Editer mon prénom</Text></Button>
+
+          <Text>Nom: {user.lastName}</Text>
+          <Button><Text>Editer mon nom de famille</Text></Button>
+
+          <Text>Nationalité: {user.nationality !== undefined ? user.nationality : 'non renseignée' }</Text>
+          <Button><Text>Changer ma nationalité</Text></Button>
+
+          <Text>Email: {user.email}</Text>
+          <Button><Text>Editer mon email</Text></Button>
+
+          <Button><Text>Changer mon mot de passe</Text></Button>
+
         </View>
-      </Content>
       </ImageBackground>
-      
+
       <Footer>
         <FooterTab style={{ backgroundColor: '#313131', }}>
           <Button onPress={() => props.navigation.navigate('Home')}>
             <Ionicons name='ios-home' size={25} color='white' />
-            <Text style={{ color: 'white', fontSize:10}}>Home</Text>
+            <Text style={{ color: 'white', fontSize: 10 }}>Home</Text>
           </Button>
           <Button onPress={() => props.navigation.navigate('Teams')}>
             <Ionicons name='ios-car' size={25} color='white' />
-            <Text style={{ color: 'white', fontSize:10 }}>Teams</Text>
+            <Text style={{ color: 'white', fontSize: 10 }}>Teams</Text>
           </Button>
           <Button onPress={() => props.navigation.navigate('Classement')}>
             <Ionicons name='ios-trophy' size={25} color='white' />
-            <Text style={{ color: 'white', fontSize:10 }}>Classement</Text>
+            <Text style={{ color: 'white', fontSize: 10 }}>Classement</Text>
           </Button >
           <Button onPress={() => props.navigation.navigate('Map')}>
             <Ionicons name='ios-map' size={25} color='white' />
-            <Text style={{ color: 'white', fontSize:10 }}>Map</Text>
+            <Text style={{ color: 'white', fontSize: 10 }}>Map</Text>
           </Button>
           <Button onPress={() => props.navigation.navigate('Medias')}>
             <Ionicons name='ios-images' size={25} color='white' />
-            <Text style={{ color: 'white', fontSize:10 }}>Medias</Text>
+            <Text style={{ color: 'white', fontSize: 10 }}>Medias</Text>
           </Button>
         </FooterTab>
       </Footer>
@@ -68,3 +87,16 @@ const styles = StyleSheet.create({
     justifyContent: "center"
   }
 })
+
+
+function mapStateToProps(state) {
+  return {
+    userFavorites: state.userFavorites,
+    userConnected: state.userConnected
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  null
+)(monCompteScreen);
