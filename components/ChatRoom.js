bc21823d404770471960cc6013899db952ddadf6
Manon Le <babyvert13@hotmail.com>
2020-12-16 12:31:06 +0100

    clean chat files

--- components/ChatRoom.js
+++ components/ChatRoom.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { View } from 'react-native';
-import { RedButtonOutline, RedButton} from '../components/rallye-lib';
 
+import { RedButtonOutline, RedButton} from '../components/rallye-lib';
 
 export default function ChatRoom(props) {
     return (
         <View>
             {props.actif === true ?
-                <RedButton title={props.room} onPress={() => { props.test() }} />
-                : <RedButtonOutline title={props.room} onPress={() => { props.test() }} />
+                <RedButton title={props.room} onPress={() => { props.buttonPress() }} />
+                : <RedButtonOutline title={props.room} onPress={() => { props.buttonPress() }} />
             }
-
         </View>
     )
 }
