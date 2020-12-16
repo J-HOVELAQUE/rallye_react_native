18a3f8d58710fadfae730fb5ea2581127e5dec07
J-HOVELAQUE <j.hovelaque@orange.fr>
2020-12-16 11:55:58 +0100

    cleaning imports

--- navigation/StackForNews.js
+++ navigation/StackForNews.js
@@ -3,8 +3,6 @@ import { createStackNavigator } from 'react-navigation-stack';
 import NewsScreen from '../screens/NewsScreen';
 import OneNewScreen from '../screens/OneNewScreen';
 
-
-
 // Stack navigation for switch on a news in full screen //
 
 const StackForNews = createStackNavigator({
