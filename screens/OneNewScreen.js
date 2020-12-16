3bd4f783b2a3097521c39734007b22f7efa2e5ae
J-HOVELAQUE <j.hovelaque@orange.fr>
2020-12-16 12:10:47 +0100

    cleaning OneNewsScreen

--- screens/OneNewScreen.js
+++ screens/OneNewScreen.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Header, Content, Button, Card, CardItem, Right, Left, Body, Container, Footer, FooterTab, Thumbnail, Text, } from 'native-base';
+import { Content, Body, Container, Text, } from 'native-base';
 import { View, Image, } from 'react-native';
 import { connect } from 'react-redux';
 
 // Importer la librairie de composants
-import { RallyeH1, RallyeH2, RallyeH3, greyDarkTa, whiteTa, icoWhite, redTa } from '../components/rallye-lib';
+import { RallyeH1, RallyeH2, RallyeH3, greyDarkTa } from '../components/rallye-lib';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 const serverUrl = 'https://powerful-earth-91256.herokuapp.com';
@@ -14,27 +14,20 @@ function NewsScreen(props) {
 
     return (
         <Container>
-
             <Content>
                 <View style={{ width: '100%', flex: 1, marginTop: 20 }}>
                     <Body>
-                        <Text style={{ fontFamily: 'Roboto_700Bold', fontSize: 20, color: greyDarkTa, }}>{props.news.title}</Text>
-
+                        <RallyeH2 style={{ fontFamily: 'Roboto_700Bold', fontSize: 20, color: greyDarkTa, }} text={props.news.title} />
 
                         <Image source={{ uri: props.news.image }} style={{ height: 220, width: 300, marginBottom: 10, flex: 1 }} />
 
                         <Text style={{ marginTop: 10 }}>{props.news.description}</Text>
-
                     </Body>
                 </View>
-
             </Content>
-
         </Container>
     )
-
 }
-
 
 function mapStateToProps(state) {
     return {
@@ -43,7 +36,6 @@ function mapStateToProps(state) {
         news: state.clickedNews
     }
 }
-
 
 function mapDispatchToProps(dispatch) {
     return {
