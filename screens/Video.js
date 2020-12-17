34038a86384e4c8e644363ffae866c335b90699f
J-HOVELAQUE <j.hovelaque@orange.fr>
2020-12-17 11:05:47 +0100

    cleaning video screen

--- screens/Video.js
+++ screens/Video.js
@@ -1,13 +1,11 @@
-import React, { useState, useCallback, useRef } from "react";
-import { Alert, View, Text, StyleSheet } from "react-native";
+import React, { useState, useCallback } from "react";
+import { Alert, View } from "react-native";
 import YoutubePlayer from "react-native-youtube-iframe";
-import { Container, Header, Content, Footer, FooterTab, Button, Left, Body, Title, Right, Image } from 'native-base';
-import { Ionicons } from '@expo/vector-icons';
-import { RedButtonLogin, RedButton, RallyeH1, RallyeH2, RallyeH3, greyDarkTa, whiteTa, icoWhite, blackTa, ProfilAvatar, greyLightTa } from '../components/rallye-lib';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { Container, Content } from 'native-base';
 
 
-export default function App(props) {
+export default function VideoScreen() {
+
   const [playing, setPlaying] = useState(false);
 
   const onStateChange = useCallback((state) => {
@@ -21,9 +19,7 @@ export default function App(props) {
 
     <Container >
 
-
       <Content style={{ flex: 1, backgroundColor: '#263238' }}>
-
         <View >
           <YoutubePlayer
             height={300}
@@ -48,27 +44,8 @@ export default function App(props) {
             onChangeState={onStateChange}
           />
         </View>
-
-
-
       </Content>
-
 
     </Container>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  backgroundVideo: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-  },
-});
-
